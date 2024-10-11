f7478690b4c66158aabe47d09a280ead25295e93
LorenzooCardoso <lorenzokcardoso@gmail.com>
2024-10-11 08:12:53 -0300

    atualizando

--- back-end/src/routes/AtvRouter.js
+++ back-end/src/routes/AtvRouter.js
@@ -3,7 +3,43 @@ const router = Router();
 const  upload   =  require('../config/multer') 
 const { storeAtv, getPosts } = require('../controller/AtvController');
 
+
+
+
+/**
+ * @swagger
+ * /store/atv:
+ *      post:
+ *          summary: Cadastro de atv
+ *          responses:
+ *              200:
+ *                  description: Cadastrar um exercicio
+ *                  content:
+ *                      appllication/json:
+ *                          schema:
+ *                              type: array
+ *                              intems:
+ *                                  type: object
+ */
 router.post('/store/atv', upload.single('file'), storeAtv);
+
+/**
+ * @swagger
+ * /post:
+ *      get:
+ *          summary: Exibicao de atv
+ *          responses:
+ *              200:
+ *                  description: Exibir a atividade
+ *                  content:
+ *                      appllication/json:
+ *                          schema:
+ *                              type: array
+ *                              intems:
+ *                                  type: object
+ */
 router.get('/posts', getPosts);
 
 module.exports = router;
+
+
--- back-end/src/routes/taskRouter.js
+++ back-end/src/routes/taskRouter.js
@@ -3,7 +3,41 @@ const router = Router();
 
 const { storeTask, Getlogin } = require('../controller/taskController'); //criado uma const tanto para o cadastro quanto para o login
 
+/**
+ * @swagger
+ * /store/task:
+ *      post:
+ *          summary: Cadastro
+ *          responses:
+ *              200:
+ *                  description: Cadastrar um usuário
+ *                  content:
+ *                      appllication/json:
+ *                          schema:
+ *                              type: array
+ *                              intems:
+ *                                  type: object
+ */
 router.post('/store/task', storeTask); //Rota pro cadastro
+
+/**
+ * @swagger
+ * /login:
+ *      get:
+ *          summary: Login
+ *          responses:
+ *              200:
+ *                  description: Retorna o login cadastrado
+ *                  content:
+ *                      appllication/json:
+ *                          schema:
+ *                              type: array
+ *                              intems:
+ *                                  type: object
+ */
 router.post('/login', Getlogin); //Rota pro login
 
 module.exports = router;
+
+
+
